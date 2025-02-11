2ff3b34ba212a0f653e1d4df3312b3e8dc543583
Piper DeHoyos <pdehoyos@uccs.edu>
2025-02-11 21:22:08 +0000

    finish task2

--- homework1/task2.py
+++ homework1/task2.py
@@ -1,3 +1,7 @@
+"""
+This program plays around with data types (int, float, string, boolean) and uses pytest to test for expected outcomes when 
+mixing data types. 
+"""
 def add(x,y):
     return x + y
 
@@ -5,15 +9,41 @@ def is_equivalent(data1,data2):
     return type(data1) == type(data2)
 
 class TestClass:
-    # test int and int
+    # testing is_equivalent func
+    # test int and int 
     def test1(self):
         assert is_equivalent(1,2) == True
 
-    # test int and bool
+    # test int and bool 
     def test2(self):
         assert is_equivalent(1, False) == False
 
-    # test bool and bool
+    # test bool and bool 
     def test3(self):
         assert is_equivalent(False, True) == True
+
+    # test int and float 
+    def test4(self):
+        assert is_equivalent(2, 3.6) == False
+
+    # test string and bool 
+    def test5(self):
+        assert is_equivalent("hello!", False) == False
+
+    # testing add func
+    # test adding int and int
+    def test6(self):
+        assert type(add(2,2)) == int
+        assert add(2,2) == 4
+    
+    # test adding float and float
+    def test7(self):
+        assert type(add(3.4, 9.5)) == float
+        assert add(3.4, 9.5) == 12.9
+
+    # test adding float and int
+    def test8(self):
+        assert type(add(1.2, 9)) == float
+        assert add(1.2, 9) == 10.2
+
 
