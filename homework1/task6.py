ad159ff9245f011798f9a5250622759bd8cc119a
Piper DeHoyos <pdehoyos@uccs.edu>
2025-02-12 17:59:09 +0000

    start task6, add py and txt file

---
+++ homework1/task6.py
@@ -0,0 +1,4 @@
+"""
+This program reads a file and counts the number of words in it using metaprogramming techniques to dynamically generate function names for pytest 
+test cases based on the filenames of the text files. Pytest is also used to verify the word count for each text file. 
+"""
