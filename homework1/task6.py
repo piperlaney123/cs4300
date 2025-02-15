a73d8a58039b272f4232ccb14d47e7c210405153
Piper DeHoyos <pdehoyos@uccs.edu>
2025-02-15 23:17:28 +0000

    finish task6

--- homework1/task6.py
+++ homework1/task6.py
@@ -6,23 +6,46 @@ metaprogramming: "programming technique where a program can modify or generate c
 modify, or create other code. In other words, metaprogramming is a way of writing programs that manipulate programs."
 - https://dev.to/karishmashukla/a-practical-guide-to-metaprogramming-in-python-691
 
-in this case, we're using metaprogramming for dynamic code generation. we'll use exec() function -- dynamic execution of Python programs 
 """
 
 import string
+import os
 
 def count_words_in_file(filename):
 
     word_count = 0
     # since using with, dont have to explicitly close file
-    with open("task6_read_me.txt", "r") as file:
+    with open(filename, "r") as file:
         for line in file:
-            for word in line.split():
-                if word not in string.punctuation:
+            for word in line.split(): # split spaces
+                if word not in string.punctuation: # ignore punctuation 
                     word_count += 1
 
     return word_count
 
 
-#print(count_words_in_file("task6_read_me.txt"))
+# set and change to appropriate directory
+directory = os.getcwd() + "/txt_files"
+os.chdir(directory)
 
+
+# go through all txt files to execute all tests
+for file in os.listdir(directory):
+    # get word count
+    word_count = count_words_in_file(file)
+    # dynamically create appropriate func name according to txt file being counted
+    # add test before file name so pytest will register it and test it
+    # also splitting at . because thats not a valid func name
+    func_name = "test_" + file.split(".")[0]
+
+    # create the test function dynamically
+    # have to pass in the file and word count for word counting func & test
+    # note that this func begins with _ ; this is bc we dont want pytest to test this func. 
+    # ((i guess i could have named it w/o test but the naming convention makes sense to me))
+    def _test_func(file=file, expected_word_count=word_count):
+        assert count_words_in_file(file) == expected_word_count
+
+    # globals() func in python: returns a dictionary representing the current global symbol table... ie, variables, functions, etc
+    # we need to be sure that the dynamically created functions are defined in the global scope so pytest can see them 
+    # attach the dynamically made function to the global scope
+    globals()[func_name] = _test_func
---
+++ homework1/txt_files/task6_read_me2.txt
@@ -0,0 +1,1 @@
+shooby do wap
